51500c51f74f32e8b177c02a23caf6332884782d
AhmedOsama198 <ahmed.osama1982002@gmail.comm>
2022-09-27 19:46:38 +0200

    Chnages

--- Server/backend/users/forms.py
+++ Server/backend/users/forms.py
@@ -1,7 +1,19 @@
 from django import forms
+from django.core import validators
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
+
+
+def validate_name(value):
+    if len(value) < 3:
+        raise ValidationError(
+            _('%(value)s is not a valid name it should be more than 2 characters'),
+            params={'value': value},
+        )
+
 
 class UserForm(forms.Form):
-    first_name = forms.CharField(max_length=20)
+    first_name = forms.CharField(max_length=20, validators=[validate_name])
     last_name = forms.CharField(max_length=20)
     email = forms.EmailField()
     password = forms.CharField(max_length=20)
